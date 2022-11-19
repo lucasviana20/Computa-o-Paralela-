26eff288e5494a7be1895693e8af8616058a7356
Guilherme Crosera Lobo <74374381+GuilhermeCLobo@users.noreply.github.com>
2022-11-19 20:39:19 -0300

    Update GMP.c

--- GMP.c
+++ GMP.c
@@ -6,14 +6,14 @@ int main(){
 
   /* The basic operation is to initialize the number */
   /* 1. Initialize the number r */
-  mpz_t r;
-  mpz_init(r);
-  mpz_set_ui(r,15);   /*r = 15 now */
+  mpf_t r;
+  mpf_init(r);
+  mpf_set_d(r,15);   /*r = 15 now */
 
-  mpz_mul_ui(r,r,10); /* r = r * 10 */
+  //mpf_mul_ui(r,r,10); /* r = r * 10 */
 
   printf (" r = ");
-  mpz_out_str(stdout,10,r);
+  mpf_out_str(stdout,10,r);
   printf ("\n");
 
 }
