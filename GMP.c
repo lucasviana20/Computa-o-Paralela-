8784f55ca6d1c46c11254457584e47c8f2a94fdf
Guilherme Crosera Lobo <74374381+GuilhermeCLobo@users.noreply.github.com>
2022-11-19 20:46:20 -0300

    Update GMP.c

--- GMP.c
+++ GMP.c
@@ -7,13 +7,13 @@ int main(){
   /* The basic operation is to initialize the number */
   /* 1. Initialize the number r */
   mpf_t r;
-  mpf_init(r);
-  mpf_set_d(r,15);   /*r = 15 now */
+  mpf_init2 (r, 256);
+  mpf_set_d(r,0.123456789123456789);   /*r = 15 now */
 
   //mpf_mul_ui(r,r,10); /* r = r * 10 */
 
   printf (" r = ");
-  mpf_out_str(stdout,10,2,r);
+  mpf_out_str(stdout,10,20,r);
   printf ("\n");
 
 }
