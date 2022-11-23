8b8ee4a3d57212823381b00d3ce185142b7ba4d0
Guilherme Crosera Lobo <74374381+GuilhermeCLobo@users.noreply.github.com>
2022-11-22 21:51:31 -0300

    Update GMP.cpp

--- GMP.cpp
+++ GMP.cpp
@@ -47,7 +47,7 @@ int main()
     gmp_printf("e= %.75Ff\n", Soma.m_Numero);
     
     Arquivo = fopen("Euler.txt", "w");
-    mpf_out_str(Arquivo, 10, 75f, Soma.m_Numero);
+    mpf_out_str(Arquivo, 10, 75F, Soma.m_Numero);
     fclose(Arquivo);
     
     return 0;
